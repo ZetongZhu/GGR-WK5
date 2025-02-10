e0ef46162d0b53bcb522d2c7efe1f7155bdf1692
ZetongZhu <zetong.zhu@mail.utoronto.ca>
2025-02-10 14:57:58 -0500

    new

--- Wk5 Exercise/script.js
+++ Wk5 Exercise/script.js
@@ -1,41 +1,68 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiemV0b25nemh1IiwiYSI6ImNtNmllamU0ejAwMzcya3BvaHl4cHdyNTEifQ.8DeoWcpHZR2z0XiEGvRoJw'; // Add default public map token from your Mapbox account
+// Token for access mapbox map
+mapboxgl.accessToken = 'pk.eyJ1IjoiemV0b25nemh1IiwiYSI6ImNtNmllamU0ejAwMzcya3BvaHl4cHdyNTEifQ.8DeoWcpHZR2z0XiEGvRoJw';
+
 const map = new mapboxgl.Map({
-    container: 'map', // map container ID
-    style: 'mapbox://styles/zetongzhu/cm6v6xrar005201s37oh0dt58', // style URL
-    center: [-79.381240, 43.657016], // starting position [lng, lat]
-    zoom: 13.17 // starting zoom level
+    container: 'map',
+    // Mapbpx style
+    style: 'mapbox://styles/zetongzhu/cm6v6xrar005201s37oh0dt58',
+    // Starting coordinates, longitude and latitude, this coordinate corresponds to Toronto downtown
+    center: [-79.381240, 43.657016],
+    // Zoom level
+    zoom: 13.17
 });
 
-map.on('load', () => {
-    // Add a data source containing GeoJSON data
-    map.addSource('uoft-data', {
-        type: 'geojson',
-        data: {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "properties": {
-                        "name": "Sidney Smith Hall"
-                    },
-                    "geometry": {
-                        "coordinates": [
-                            -79.39865237301687,
-                            43.662343395037766
-                        ],
-                        "type": "Point"
-                    }
+// Rain effect, code adapted from Mapbox
+map.on('style.load', () => {
+    map.setRain({
+        // Rain density
+        density: [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            11,
+            0.0,
+            13,
+            0.5
+        ],
+
+        intensity: 1.0,
+        color: '#a8adbc',
+        opacity: 0.7,
+        vignette: [
+            'interpolate',
+            ['linear'],
+            ['zoom'],
+            11,
+            0.0,
+            13,
+            1.0
+        ],
+        'vignette-color': '#464646',
+        direction: [0, 80],
+        'droplet-size': [2.6, 18.2],
+        'distortion-strength': 0.7,
+        'center-thinning': 0
+
+        map.on('load', () => {
+            map.addSource('subway-lines', {
+                type: 'geojson',
+                data: 'toronto_subway.geojson' // Make sure this file is accessible in your project directory
+            });
+
+            map.addLayer({
+                id: 'subway-lines-layer',
+                type: 'line',
+                source: 'subway-lines',
+                layout: {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                paint: {
+                    'line-color': ['get', 'ROUTE_COLOR'], // Uses color directly from GeoJSON
+                    'line-width': 4
                 }
-            ]
-        }
+            });
+        })
+
     });
-    map.addLayer({
-        'id': 'uoft-pnt',
-        'type': 'circle',
-        'source': 'uoft-data',
-        'paint': {
-            'circle-radius': 6,
-            'circle-color': '#B42222'
-        }
-    });
-})
+});
